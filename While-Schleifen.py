b8e5cfbb1dae935f548ff18787b6ad83dc1e784e
Bagiwka <sagivka2006@gmail.com>
2023-11-07 17:05:30 +0100

    update

--- While-Schleifen.py
+++ While-Schleifen.py
@@ -1,7 +1,7 @@
 import re
 
 def ist_gueltige_seriennummer(seriennummer):
-    return bool(re.match(r'^[a-zA-Z0-9]{10}$', seriennummer))
+    return bool(re.match(r'^[a-zA-Z0-9]{12}$', seriennummer))
 
     #re.match == compare
     #^ == Anfang vom String
@@ -9,6 +9,30 @@ def ist_gueltige_seriennummer(seriennummer):
     #{10} == wiederhole 10 mal
     #$ == End vom String
     #, seriennummer == führe mit seriennummer aus
+
+def prüfziffer_prüfer(seriennummer):
+    i=0
+    total = 0
+    while i<11:
+        if i<2:
+            number = ord(seriennummer[i].lower()) - 96
+        else:
+            number = int(seriennummer[i])
+        total = total + number
+        i+=1
+    total = total % 9
+    total = 7 - total
+    if total == 0:
+        total = 9
+    elif total == -1:
+        total = 8
+    if(int(seriennummer[11]) == total):
+        returnvar = True
+    else:
+        returnvar = False
+    return returnvar
+    
+        
 
 while True:
     zahl = None
@@ -26,10 +50,12 @@ while True:
             print("Das Passwort muss mindestens 8 Zeichen lang sein.")
 
     seriennummer = None
-    while seriennummer is None or not ist_gueltige_seriennummer(seriennummer):
+    while seriennummer is None or not ist_gueltige_seriennummer(seriennummer) or not prüfziffer_prüfer(seriennummer):
         seriennummer = input("Geben Sie die Seriennummer eines Geldscheins ein: ")
         if not ist_gueltige_seriennummer(seriennummer):
             print("Ungültige Seriennummer. Die Seriennummer muss aus 10 alphanumerischen Zeichen bestehen.")
+        elif not prüfziffer_prüfer(seriennummer):
+            print("Ungültige Prüfziffer.")
 
     print("Eingaben sind korrekt!")
 
